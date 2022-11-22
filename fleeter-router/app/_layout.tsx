96938ea8d80287a2827e7079dbc1da86d4764b1c
Oliver Kwast <oliver.kwast@bitglow.de>
2022-11-22 20:46:54 +0100

    ☀️ / 🌕 Added theming support

--- fleeter-router/app/_layout.tsx
+++ fleeter-router/app/_layout.tsx
@@ -1,10 +1,15 @@
-import { Stack } from "expo-router";
+import { DarkTheme } from "@react-navigation/native";
+import { RootContainer, Stack } from "expo-router";
 import React from "react";
+import { useColorScheme } from "react-native";
 import { StoreProvider } from "../store/store";
 
 export default function Root() {
+  const colorScheme = useColorScheme();
+
   return (
     <StoreProvider>
+      <RootContainer theme={colorScheme === "light" ? undefined : DarkTheme} />
       <Stack screenOptions={{ headerShown: false, presentation: "modal" }} />
     </StoreProvider>
   );
