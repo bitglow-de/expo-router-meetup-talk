73320a3d760154af401f6be5097203b874db9925
Jonathan Bones <jonathan.bones@bitglow.de>
2022-12-04 23:50:45 +0100

    ✨Render feed in tab bar

--- fleeter-router/app/_layout.tsx
+++ fleeter-router/app/_layout.tsx
@@ -1,8 +1,9 @@
 import { DarkTheme } from "@react-navigation/native";
-import { RootContainer, Stack } from "expo-router";
+import { RootContainer, Tabs } from "expo-router";
 import React from "react";
 import { useColorScheme } from "react-native";
 import { StoreProvider } from "../store/store";
+import { renderHomeIcon } from "../utils/tabs";
 
 export default function Root() {
   const colorScheme = useColorScheme();
@@ -10,7 +11,12 @@ export default function Root() {
   return (
     <StoreProvider>
       <RootContainer theme={colorScheme === "light" ? undefined : DarkTheme} />
-      <Stack />
+      <Tabs screenOptions={{ headerShown: false }} initialRouteName="(feed)">
+        <Tabs.Screen
+          name="(feed)"
+          options={{ title: "Home", tabBarIcon: renderHomeIcon }}
+        />
+      </Tabs>
     </StoreProvider>
   );
 }
