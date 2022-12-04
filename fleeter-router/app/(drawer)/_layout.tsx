72edea5fbebc42aca9d3dad09d93cd8a6b176a60
Jonathan Bones <jonathan.bones@bitglow.de>
2022-12-05 00:24:33 +0100

    ✨Add custom drawer content

--- fleeter-router/app/(drawer)/_layout.tsx
+++ fleeter-router/app/(drawer)/_layout.tsx
@@ -1,9 +1,36 @@
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+import { DrawerContentComponentProps } from "@react-navigation/drawer/lib/typescript/src";
 import { Drawer } from "expo-router/drawer";
 import React from "react";
+import { StyleSheet, Text } from "react-native";
 
-export default function DrawerLayout() {
+const FleeterDrawerContent: React.FC<DrawerContentComponentProps> = (props) => {
   return (
-    <Drawer screenOptions={{ headerShown: false }}>
+    <DrawerContentScrollView {...props}>
+      <Text style={styles.heading}>Menu</Text>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 32,
+    fontWeight: "700",
+    lineHeight: 40,
+    paddingLeft: 10,
+  },
+});
+
+export default function Layout() {
+  return (
+    <Drawer
+      screenOptions={{ headerShown: false }}
+      drawerContent={FleeterDrawerContent}
+    >
       <Drawer.Screen name="(tabs)" options={{ drawerLabel: "Feed" }} />
       <Drawer.Screen
         name="my-profile"
